26cb5089884b1b8b6a588f73d4ec07b076efc718
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-22 10:22:58 -0400

    changed db name and messed with page format

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,13 +19,17 @@ const Home = () => {
         </nav>
       </header>
       <section>
-        <h2>Featured Playlists</h2>
+        <h2> Featured Tracks</h2>
         <div>
+          {/* tracks */}
+        </div>
+        <h2>Featured Playlists</h2>
+        <div id="playlists">
           {/* TODO: Add featured playlists here */}
         </div>
       </section>
       <footer>
-        <div>
+        <div id='now-playing'>
           {/* TODO: Add Now Playing bar here */}
         </div>
       </footer>
