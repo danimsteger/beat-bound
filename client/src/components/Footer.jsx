20e6222a0bd09554eeb4d29d392423d976b5b8be
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-26 12:01:58 -0400

    jsx changes

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+      <>
+        <div>
+            Does this work hullo
+        </div>
+      </>
+    )}
+
+    export default Footer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App.jsx';
-// import { BrowserRouter as Router } from 'react-router-dom';
-
 import ReactDOM from "react-dom/client";
-// import './index.css'
+import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NoMatch from "./pages/NoMatch.jsx";
@@ -12,8 +7,6 @@ import App from "./App.jsx";
 import Home from "./pages/Home";
 import Search from "./pages/Search.jsx";
 import ArtistPage from "./pages/ArtistPage.jsx";
-// import EventPage from './pages/EventPage.jsx'
-// import FriendsFeed from './pages/FriendsFeed.jsx'
 import Profile from "./pages/Profile.jsx";
 const router = createBrowserRouter([
   {
@@ -33,12 +26,6 @@ const router = createBrowserRouter([
         path: "/ArtistPage/:artistId",
         element: <ArtistPage />,
       },
-      //   path: '/EventPage',
-      //   element: <EventPage />
-      // },{
-      //   path: '/FriendsFeed',
-      //   element: <FriendsFeed />
-      // },
       {
         path: "/Profile",
         element: <Profile />,
@@ -51,12 +38,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
 
-// const Main = () => {
-//   return (
-//     <Router>
-//       <App />
-//     </Router>
-//   );
-// };
-
-// ReactDOM.render(<Main />, document.getElementById('root'));
