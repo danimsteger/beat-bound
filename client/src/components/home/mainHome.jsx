1b25ea62c86681d99a6a86fa06b80845d5cefe6d
danimsteger <daniellestegertutoring@gmail.com>
2024-08-26 22:44:40 -0400

    search page button linked on home page

--- client/src/components/home/mainHome.jsx
+++ client/src/components/home/mainHome.jsx
@@ -1,5 +1,8 @@
 import customTheme from "../../styles/customTheme";
 import { Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
 const MainHome = () => {
   return (
     <div
@@ -28,8 +31,17 @@ const MainHome = () => {
         <h3 className=" concert-one-regular">Discover New Music</h3>
         <h4 className="concert-one-regular">& Save it for Later</h4>
       </div>
-      <div>
-        <Button></Button>
+      <div style={{ margin: 50 }}>
+        <Link to={`/search-page`}>
+          <Button
+            size="large"
+            style={{ fontSize: 30, color: customTheme.token.colorSecondary }}
+            className="bungee-regular"
+          >
+            {" "}
+            Explore {<SearchOutlined />}
+          </Button>
+        </Link>
       </div>
     </div>
   );
